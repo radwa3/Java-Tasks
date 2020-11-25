c291e741304b4fc759773f444aabc1daac179086
radwa3 <75011323+radwa3@users.noreply.github.com>
2020-11-25 15:57:12 +0200

    Add files via upload

---
+++ NumberMulti.java
@@ -0,0 +1,10 @@
+
+public class NumberMulti {
+	public static void main(String[] args){
+		for(int i =1 ; i<=12;i++){
+			int Multi = 2*i;
+			System.out.print( "2*"+i+"="+Multi+"\n");
+		}
+	}
+
+}
